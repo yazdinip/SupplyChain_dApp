34c71d6302e84e59e955872e7c9f67a031504cbf
Pedram Yazdinia <yazdinip@mcmaster.ca>
2022-07-10 22:51:15 -0400

    added few constructs

--- contracts/supplyChain.sol
+++ contracts/supplyChain.sol
@@ -3,5 +3,23 @@
 pragma solidity >=0.8.0;
 
 contract supplyChain{
+    uint32 public product_id = 0;   // Product ID
+    uint32 public participant_id = 0;   // Participant ID
+    uint32 public owner_id = 0;   // Ownership ID    
+
+    struct participant{
+        string userName;
+        string password;
+        string participantType;
+        address participantAddress;
+    }
     
+    struct product{
+        string modelNumber;
+        string partNumber;
+        string serialNumber;
+        address productOwner;
+        uint32 cost;
+        uint32 mfgTimeStamp;
+    }
 }
