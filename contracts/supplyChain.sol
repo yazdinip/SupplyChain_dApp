27300e8b79d5aed6fb2a6e5b55d38ca16ebbdbd7
Pedram Yazdinia <yazdinip@mcmaster.ca>
2022-07-11 22:15:38 -0400

    added addParticipant

--- contracts/supplyChain.sol
+++ contracts/supplyChain.sol
@@ -34,4 +34,16 @@ contract supplyChain{
     mapping(uint32 => product) public products;
 
     event TransferOwnership(uint32 prdouctId);
+
+    function addParticipant(string memory _name, string memory _pass, address _pAdd, string memory _pType) public returns (uint32){
+        uint32 userId = participant_id++;
+        participants[userId].userName = _name;
+        participants[userId].password = _pass;
+        participants[userId].participantAddress = _pAdd;
+        participants[userId].participantType = _pType;
+
+        return userId;
+    }
+
+    
 }
